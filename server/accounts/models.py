d3b5ee4778a9e90d6e1b6b2c44c0aa2f18af8593
uuljan <u.aitnazarova@gmail.com>
2021-08-21 16:57:28 +0600

    Created model Profile in accounts, made migrations

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile')
+    phone_regex = RegexValidator(regex=r'^\+996\w{9}$',
+                                 message="Phone number must be entered in the format: '+996xxxxxxxxx'. Up to 12 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.user}: {self.phone_number}'
