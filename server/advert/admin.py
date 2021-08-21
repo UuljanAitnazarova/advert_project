606613be14781301f14f9f8c45a5755072b391fb
uuljan <u.aitnazarova@gmail.com>
2021-08-21 12:08:56 +0600

    Registered Advert model in admin.py

--- server/advert/admin.py
+++ server/advert/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from advert.models import Advert
+
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'description', 'image', 'price', 'author', 'created_date', 'modified_date', 'post_date', 'moderated']
+    list_filter = ['title', 'category']
+    search_fields = ['title']
+    fields = ['title', 'category', 'description', 'image', 'price', 'author', 'moderated']
+    readonly_fields = ['id', 'created_date', 'modified_date', 'post_date']
+
+
+admin.site.register(Advert, AdvertAdmin)
+
+
+
